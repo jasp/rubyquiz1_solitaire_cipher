58b8bb2faa41b7b1eb97e0981f0273d2621ddb13
Jakob Skov-Pedersen <jasp@thecrypt.dk>
2010-08-31 22:48:20 +0200

    Start with pendign specs

---
+++ spec/quiz1/encoder_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+module Quiz1
+  describe Encoder do
+    describe '#step1' do
+      it "converts an input string to all uppercase"
+      it "removes non-letters from input string"
+      it "splits message into five character groups"
+      it "pads Xs to the last group to get to 5 characters"
+    end
+  end
+end
