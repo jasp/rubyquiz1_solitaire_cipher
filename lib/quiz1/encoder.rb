6eabab900a3ac075e0265d6284e329cdff3c3893
Jakob Skov-Pedersen <jasp@thecrypt.dk>
2010-08-31 23:12:13 +0200

    Two more examples run
    For first step in encoder algorithm. Still 2 are left for step1.

--- lib/quiz1/encoder.rb
+++ lib/quiz1/encoder.rb
@@ -1,9 +1,13 @@
 module Quiz1
   class Encoder
-    def initialize(output)
+    def initialize(output = nil)
     end
 
     def encode(message)
+    end
+
+    def step1(message)
+      message.upcase.gsub(/[^A-Z]/, '')
     end
   end
 end
--- spec/quiz1/encoder_spec.rb
+++ spec/quiz1/encoder_spec.rb
@@ -3,8 +3,17 @@ require 'spec_helper'
 module Quiz1
   describe Encoder do
     describe '#step1' do
-      it "converts an input string to all uppercase"
-      it "removes non-letters from input string"
+      it "converts an input string to all uppercase" do
+        encoder = Encoder.new
+        encoder.step1("aBcDe").should match(/^ABCDE/)
+        encoder.step1("abcABC").should match(/^[A-Z ]*$/)
+      end
+
+      it "removes non-letters from input string" do
+        encoder = Encoder.new
+        encoder.step1(".,;:%1234567890^&#!'\"\\|@$*()-_=+{}[]?/`~<> ABCDEF").should match(/^[A-Z ]*$/)
+      end
+
       it "splits message into five character groups"
       it "pads Xs to the last group to get to 5 characters"
     end
